2e470fecd00bc6a231125657aa918749cd8ca474
fraxken <gentilhomme.thomas@gmail.com>
2017-08-30 02:32:50 +0200

    Bump to 1.7.9 npm

--- test/test.js
+++ test/test.js
@@ -1,34 +1,54 @@
-const assert = require('assert');
-const {Server,SocketsPools,Client,Message} = require('../index.js');
+const test = require('japa');
+const events = require('events');
 
-const WSServer = new Server({port: 3000});
+class t2 extends events {}
+const eWrapper = new t2(); 
 
-describe('uVyper Server properties tests...', function() {
-    describe('WSServer.id',function() {
-        it('should be a string', function() {
-            assert.strictEqual('string', typeof(WSServer.id));
-        });
-    });
+const {Server} = require('../index.js');
 
-    describe('WSServer.ssl',function() {
-        it('should be a boolean', function() {
-            assert.strictEqual('boolean', typeof(WSServer.ssl));
-        });
-    });
-
-    describe('WSServer.sockets',function() {
-        it('should be instanceof SocketsPools', function() {
-            assert.strictEqual( true , WSServer.sockets instanceof SocketsPools);
-        });
-    });
-
-    describe('WSServer.wss',function() {
-        it('should be instanceof Client.Server', function() {
-            assert.strictEqual( true , WSServer.wss instanceof Client.Server);
+let WSServer;
+process.nextTick(function() {
+    setImmediate(function() {
+        WSServer = new Server({port: 3000});
+        WSServer.wss.on('listening',function() {
+            eWrapper.emit('listening');
         });
     });
 });
 
-describe('uVyper Server events tests...', function() {
-    
-});
+test('Server listening',(assert,done) => {
+    eWrapper.on('listening',function() {
+        done();
+    });
+}).timeout(1000);
+
+/*function testEvent(strTitle,fn) {
+    return new Promise((resolve,reject) => {
+        console.log(`TEST:: ${strTitle}`);
+        const timeOut = setTimeout(function() {
+            reject('timeout');
+        },5000);
+        const done = function() {
+            clearTimeout(timeOut);
+            resolve();
+        };
+        process.nextTick( function() {
+            fn(done);
+        });
+    });
+}
+
+testEvent('Server listening',function(done) {
+    WSServer.wss.on('listening',function() {
+        console.log('listening triggered!');
+        done();
+    });
+})
+.then( () => {
+    console.log('Resolved!');
+    WSServer.close();
+    process.exit(0);
+})
+.catch( E => {
+    throw E;
+}); */
